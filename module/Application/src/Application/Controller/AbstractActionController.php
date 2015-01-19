fdfeecf98d08c9d95680af74a9affc478e8e027a
Sam Xiao <samxiaotj@gmail.com>
2015-01-19 12:59:47 +0800

    [Member] update profile page

---
+++ module/Application/src/Application/Controller/AbstractActionController.php
@@ -0,0 +1,9 @@
+<?php
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController as ZFAAC;
+
+class AbstractActionController extends ZFAAC
+{
+}
+
--- public/js/main/member/profile.js
+++ public/js/main/member/profile.js
@@ -1,23 +1,43 @@
 var tmy_member_list = {
-	init : function() {
-		$('.chosen-select').chosen({allow_single_deselect:true}); 
+    init : function() {
+        $('.chosen-select').chosen({
+            allow_single_deselect : true
+        });
 
-		$('#modal-form').on('shown.bs.modal', function () {
-			$(this).find('.chosen-container').css('width' , '100%');
-			$(this).find('.chosen-container').each(function(){
-				$(this).find('a:first-child').css('width' , '100%');
-				$(this).find('.chosen-drop').css('width' , '100%');
-				$(this).find('.chosen-search input').css('width' , '100%');
-			});
-		})
-	},
-	bindEvent: function(){
-		$(".goods-chosen-select").on('change', function(){
-			
-		})
-	},
+        $('#modal-form').on('shown.bs.modal', function() {
+            $(this).find('.chosen-container').css('width', '100%');
+            $(this).find('.chosen-container').each(function() {
+                $(this).find('a:first-child').css('width', '100%');
+                $(this).find('.chosen-drop').css('width', '100%');
+                $(this).find('.chosen-search input').css('width', '100%');
+            });
+        });
+        this.bindEvent();
+    },
+    bindEvent : function() {
+        $(".goods-chosen-select").on('change', function() {
+            var type = '';
+            $(".goods-chosen-select option:selected").each(function() {
+                type = $(this).data('goods-type');
+            });
+            console.log(type);
+            if(type == '时间卡'){
+                $(".time-card-only").show();
+                $(".count-card-only").hide();
+            }else{
+                $(".time-card-only").hide();
+                $(".count-card-only").show();
+            }
+        });
+        $("#btn-submit-buy-modal").on("click", function (){
+            var data = $("#buy_goods_form").serialize();
+            $.getJSON( "ajax/test.json", function( data ) {
+              
+              });
+        });
+    },
 };
 
 $(function() {
-	tmy_member_list.init();
+    tmy_member_list.init();
 });
